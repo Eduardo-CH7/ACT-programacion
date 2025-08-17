35c3adffb06ef8e26acce4c766baed9fc30c3c94
Eduardo-CH7 <eduardok211107@gmail.com>
2025-08-16 23:16:16 -0600

    Add files via upload

---
+++ 2.- Datos de un producto.py
@@ -0,0 +1,7 @@
+# Definición de variables
+nombre_producto = "Laptop"
+precio_producto = 12500.50
+
+# Mostrar los datos en pantalla de forma clara
+print("Nombre del producto:", nombre_producto)
+print("Precio del producto: $", precio_producto)
