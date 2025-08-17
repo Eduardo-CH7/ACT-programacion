24b150c12cc3367310db27003a177d6553a66cea
Eduardo-CH7 <eduardok211107@gmail.com>
2025-08-16 23:16:35 -0600

    Update 2.- Datos de un producto.py

--- 2.- Datos de un producto.py
+++ 2.- Datos de un producto.py
@@ -2,6 +2,7 @@
 nombre_producto = "Laptop"
 precio_producto = 12500.50
 
-# Mostrar los datos en pantalla de forma clara
+
 print("Nombre del producto:", nombre_producto)
 print("Precio del producto: $", precio_producto)
+
