c6f113d0bfcaa26d97ec9cb6e45b99427ceb5782
Eduardo-CH7 <eduardok211107@gmail.com>
2025-08-16 23:20:37 -0600

    Update 4.- Operadores relacionales.py

--- 4.- Operadores relacionales.py
+++ 4.- Operadores relacionales.py
@@ -2,10 +2,11 @@
 a = 2
 b = 7
 
-# Aplicar operadores relacionales y mostrar los resultados
+
 print("a == b:", a == b)
 print("a != b:", a != b)
 print("a > b:", a > b)
 print("a < b:", a < b)
 print("a >= b:", a >= b)
 print("a <= b:", a <= b)
+
