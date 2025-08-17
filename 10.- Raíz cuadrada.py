2ea8fee85c4ebe058847a1c723e908d69a634915
Eduardo-CH7 <eduardok211107@gmail.com>
2025-08-16 23:26:33 -0600

    Add files via upload

---
+++ 10.- Raíz cuadrada.py
@@ -0,0 +1,5 @@
+import math   
+
+resultado = math.sqrt(10)
+
+print("La raíz cuadrada de 10 es:", resultado)
---
+++ 9.- Tipos de datos.py
@@ -0,0 +1,9 @@
+# Definimos las variables con diferentes tipos de datos
+nombre = 'Pedro'   
+x = 200            
+y = 15.4           
+
+
+print(type(nombre))  
+print(type(x))       
+print(type(y))       
