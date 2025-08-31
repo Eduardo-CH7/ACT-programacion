7625e78e6ff68068ac9fe61f293242f82b506a7f
Eduardo-CH7 <eduardok211107@gmail.com>
2025-08-31 15:47:53 -0600

    Add files via upload

---
+++ problema 12.py
@@ -0,0 +1,5 @@
+nombre = input("¿Cuál es tu nombre? ")
+edad = int(input("¿Cuántos años tienes? "))
+año_actual = 2025
+año_nacimiento = año_actual - edad
+print(f"Hola, {nombre}. Probablemente naciste en el año {año_nacimiento}.")
